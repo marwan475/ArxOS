b0983fde5a298cf3f9a0dd81e611554ff31b619a
Marwan <marwanmo475@gmail.com>
2025-10-26 20:24:56 -0400

    Hello world bootloader

--- Bootloader/bootloader.cpp
+++ Bootloader/bootloader.cpp
@@ -4,6 +4,13 @@ extern "C"
 {
     EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
     {
+
+        EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConsoleOutput = SystemTable->ConOut;
+
+        ConsoleOutput->Reset(ConsoleOutput, false);
+        ConsoleOutput->ClearScreen(ConsoleOutput);
+        ConsoleOutput->OutputString(ConsoleOutput, u"Welcome to ArxOS Bootloader\n");
+
         while(true){}
 
         return 0;
--- Bootloader/uefi.hpp
+++ Bootloader/uefi.hpp
@@ -2,14 +2,12 @@
 
 #include <stdint.h>
 
-// Table 2.4
 #define IN
 #define OUT
 #define OPTIONAL
 #define CONST const
-#define EFIAPI __attribute__((ms_abi)) // Calling Spec UEFI Spec 2.3
+#define EFIAPI __attribute__((ms_abi)) 
 
-// Data Types UEFI Spec 2.3.1
 typedef uint8_t  BOOLEAN;
 typedef int64_t  INTN;
 typedef uint64_t UINTN;
@@ -41,7 +39,146 @@ typedef void*  EFI_EVENT;
 typedef UINT64 EFI_LBA;
 typedef UINTN  EFI_TPL;
 
-typedef void EFI_SYSTEM_TABLE;
+// Status Codes
+#define EFI_SUCCESS 0ULL
+
+// Text Input
+typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+typedef EFI_STATUS(EFIAPI* EFI_INPUT_RESET)(IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL* This,
+                                            IN BOOLEAN ExtendedVerification);
+
+typedef struct
+{
+    UINT16 ScanCode;
+    CHAR16 UnicodeChar;
+} EFI_INPUT_KEY;
+
+typedef EFI_STATUS(EFIAPI* EFI_INPUT_READ_KEY)(IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL* This,
+                                               OUT EFI_INPUT_KEY*                 Key);
+
+typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL
+{
+    EFI_INPUT_RESET    Reset;
+    EFI_INPUT_READ_KEY ReadKeyStroke;
+    EFI_EVENT          WaitForKey;
+} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+// Text Output
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_RESET)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+                                           IN BOOLEAN ExtendedVerification);
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_STRING)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+                                            IN CHAR16*                          String);
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_QUERY_MODE)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+                                                IN UINTN ModeNumber, OUT UINTN* Columns,
+                                                OUT UINTN* Rows);
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_SET_MODE)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+                                              IN UINTN                            ModeNumber);
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_SET_ATTRIBUTE)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+                                                   IN UINTN                            Attribute);
+
+#define EFI_BLACK 0x00
+#define EFI_BLUE 0x01
+#define EFI_GREEN 0x02
+#define EFI_CYAN 0x03
+#define EFI_RED 0x04
+#define EFI_MAGENTA 0x05
+#define EFI_BROWN 0x06
+#define EFI_LIGHTGRAY 0x07
+#define EFI_BRIGHT 0x08
+#define EFI_DARKGRAY 0x08
+#define EFI_LIGHTBLUE 0x09
+#define EFI_LIGHTGREEN 0x0A
+#define EFI_LIGHTCYAN 0x0B
+#define EFI_LIGHTRED 0x0C
+#define EFI_LIGHTMAGENTA 0x0D
+#define EFI_YELLOW 0x0E
+#define EFI_WHITE 0x0F
+
+#define EFI_BACKGROUND_BLACK 0x00
+#define EFI_BACKGROUND_BLUE 0x10
+#define EFI_BACKGROUND_GREEN 0x20
+#define EFI_BACKGROUND_CYAN 0x30
+#define EFI_BACKGROUND_RED 0x40
+#define EFI_BACKGROUND_MAGENTA 0x50
+#define EFI_BACKGROUND_BROWN 0x60
+#define EFI_BACKGROUND_LIGHTGRAY 0x70
+
+#define EFI_TEXT_ATTR(Foreground, Background) ((Foreground) | ((Background) << 4))
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_CLEAR_SCREEN)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This);
+
+typedef EFI_STATUS(EFIAPI* EFI_TEXT_SET_CURSOR_POSITION)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This,
+                                                         IN UINTN Column, IN UINTN Row);
+
+typedef struct
+{
+    INT32   MaxMode;
+    INT32   Mode;
+    INT32   Attribute;
+    INT32   CursorColumn;
+    INT32   CursorRow;
+    BOOLEAN CursorVisible;
+} SIMPLE_TEXT_OUTPUT_MODE;
+
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+{
+    EFI_TEXT_RESET               Reset;
+    EFI_TEXT_STRING              OutputString;
+    void*                        TestString;
+    EFI_TEXT_QUERY_MODE          QueryMode;
+    EFI_TEXT_SET_MODE            SetMode;
+    EFI_TEXT_SET_ATTRIBUTE       SetAttribute;
+    EFI_TEXT_CLEAR_SCREEN        ClearScreen;
+    EFI_TEXT_SET_CURSOR_POSITION SetCursorPosition;
+    void*                        EnableCursor;
+    SIMPLE_TEXT_OUTPUT_MODE*     Mode;
+} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+typedef struct
+{
+} EFI_RUNTIME_SERVICES;
+
+typedef struct
+{
+} EFI_BOOT_SERVICES;
+
+typedef struct
+{
+} EFI_CONFIGURATION_TABLE;
+
+typedef struct
+{
+    UINT64 Signature;
+    UINT32 Revision;
+    UINT32 HeaderSize;
+    UINT32 CRC32;
+    UINT32 REserved;
+} EFI_TABLE_HEADER;
+
+// System Table
+typedef struct
+{
+    EFI_TABLE_HEADER                 Hdr;
+    CHAR16*                          FirmwareVendor;
+    UINT32                           FirmwareRevision;
+    EFI_HANDLE                       ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL*  ConIn;
+    EFI_HANDLE                       ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut;
+    EFI_HANDLE                       StandardErrorHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* StdErr;
+    EFI_RUNTIME_SERVICES*            RuntimeServices;
+    EFI_BOOT_SERVICES*               BootServices;
+    UINTN                            NumberOfTableEntries;
+    EFI_CONFIGURATION_TABLE*         ConfigurationTable;
+} EFI_SYSTEM_TABLE;
 
 typedef EFI_STATUS(EFIAPI* EFI_IMAGE_ENTRY_POINT)(IN EFI_HANDLE        ImageHandle,
                                                   IN EFI_SYSTEM_TABLE* SystemTable);
